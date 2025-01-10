28c377af95d3ad8c74860799d69eda57639bba42
Sonu <bhaarat.mohanta@gmail.com>
2025-01-09 20:57:14 -0600

    feat: nuclear drop

---
+++ scripts/enemy/classic_world/drops/nuclear_drop.gd
@@ -0,0 +1,31 @@
+extends Node2D
+
+var animated_sprite : AnimatedSprite2D
+var drop_component : Node2D
+func _ready() -> void:
+	animated_sprite = get_node("AnimatedSprite2D")
+	drop_component = get_node("drop_component")
+	animated_sprite.play("idle")
+	drop_component.timer_length += randi() % 15
+	drop_component.fall_speed += randi() % 210
+
+func timer_timeout_event():
+	self.rotation_degrees = 0
+	$explosion_hitbox/CollisionShape2D.set_deferred("disabled", false)
+	animated_sprite.play("explosion")
+	$StaticBody2D/CollisionShape2D.set_deferred("disabled", true)
+	$CPUParticles2D.emitting = true
+
+func _on_explosion_hitbox_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		body.set_debuff("flipped_controls")
+
+# When the player attacks this object
+func attacked():
+	self.get_parent().get_parent().get_parent().score += 1
+	drop_component.delete_timer.start()
+	drop_component.isActive = false #freezes the object
+	$StaticBody2D.set_deferred("disable_mode",true) # disables the eating hitboxes
+	$StaticBody2D/CollisionPolygon2D.set_deferred("disabled",true)
+	drop_component.active_timer.stop()
+	drop_component.active_timer.emit_signal("timeout")
