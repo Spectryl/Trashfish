50e95e3bcc270804518dc0a70d23de6c9457375a
SonuTheNecro <156054782+SonuTheNecro@users.noreply.github.com>
2025-01-27 17:55:30 -0600

    add: epic buttons w/ epic colors
    also a beach world text issue

--- scripts/world/main_menu/main_menu_buttons.gd
+++ scripts/world/main_menu/main_menu_buttons.gd
@@ -1,6 +1,13 @@
 extends Control
 @onready var menu : Control = get_parent()
+@onready var start_button   : TextureButton  = $MarginContainer/VBoxContainer/start_button
+@onready var options_button : TextureButton  = $MarginContainer/VBoxContainer/options_button
+@onready var controls_button : TextureButton = $MarginContainer/VBoxContainer/controls_button
+@onready var quit_button : TextureButton     = $MarginContainer/VBoxContainer/quit_button
 
+var hover_color : Color = Color(255,0,0,255) # Color when we hover a button
+var reset_color : Color = Color(1,1,1,1)     # Color when we stop hovering, this can be forced to be default color
+#region Button_Press
 func _on_start_button_pressed() -> void:
 	global.world_master.change_world(2)
 	load_save_data()
@@ -13,6 +20,7 @@ func _on_controls_button_pressed() -> void:
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
+#endregion
 
 # Will create a save file if the player does not have one, otherwise it does nothing really.
 func load_save_data():
@@ -25,3 +33,29 @@ func load_save_data():
 		print("No Save file found!")
 	else:
 		print("Save file found")
+
+func _on_start_button_mouse_entered() -> void:
+	start_button.set_deferred("modulate", hover_color)
+
+#region Hovering a button Code
+func _on_start_button_mouse_exited() -> void:
+	start_button.set_deferred("modulate", reset_color)
+
+func _on_options_button_mouse_entered() -> void:
+	options_button.set_deferred("modulate", hover_color)
+
+func _on_options_button_mouse_exited() -> void:
+	options_button.set_deferred("modulate", reset_color)
+
+func _on_controls_button_mouse_entered() -> void:
+	controls_button.set_deferred("modulate", hover_color)
+
+func _on_controls_button_mouse_exited() -> void:
+	controls_button.set_deferred("modulate", reset_color)
+
+func _on_quit_button_mouse_entered() -> void:
+	quit_button.set_deferred("modulate", hover_color)
+
+func _on_quit_button_mouse_exited() -> void:
+	quit_button.set_deferred("modulate", reset_color)
+#endregion
