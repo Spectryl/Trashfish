760c69d47d706273f190f9a6af0b9c06df35e628
SonuTheNecro <156054782+SonuTheNecro@users.noreply.github.com>
2025-02-22 22:44:15 -0600

    add: bounce_shine shader
    WHY DO WEE NEED TO IBBREED THE SHADERSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS

--- scripts/world/main_menu/world_select.gd
+++ scripts/world/main_menu/world_select.gd
@@ -8,17 +8,19 @@ var electro              : TwoClickTextureButton
 var state : int = 0
 var hover_color : Color = Color8(0,255,255,255) # Color when we hover a button
 var reset_color : Color = Color8(255,255,255,255)     # Color when we stop hovering, this can be forced to be default color
-var bounce_shader = load("res://resources/shaders/bounce_shader.res")
+var bounce_shine_shader = load("res://resources/buttons/world_icon.res")
 func _ready() -> void:
 	beach = TwoClickTextureButton.new()
 	beach.texture_normal = load("res://resources/buttons/beach_world_icon.tres")
 	beach.left_click .connect(_on_beach_left_click_pressed)
 	beach.right_click.connect(_on_beach_right_click_pressed)
-	beach.material = bounce_shader
+	beach.material = bounce_shine_shader 
 	beach.material.set_shader_parameter("do_abs", false)
 	beach.material.set_shader_parameter("do_quantize", true)
 	beach.material.set_shader_parameter("sine_amplitude", Vector2(0,20))
 	beach.material.set_shader_parameter("sine_speed", Vector2(0,3))
+
+	
 	worlds.add_child(beach)
 
 	volcano = TwoClickTextureButton.new()
