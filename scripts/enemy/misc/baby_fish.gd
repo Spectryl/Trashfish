e57671f5eb999258a086f473d59e4c545314bf59
SonuTheNecro <156054782+SonuTheNecro@users.noreply.github.com>
2025-02-23 16:06:50 -0600

    test: babyfish (read desc)
    add: babyfish art
    babyfish movement script
    babyfish navigation region
    babyfish animations

---
+++ scripts/enemy/misc/baby_fish.gd
@@ -0,0 +1,39 @@
+extends CharacterBody2D
+
+@onready var head : AnimatedSprite2D              = $head
+@onready var tail : Sprite2D                      = $tail
+@onready var garbage_can : Sprite2D               = $garbage_can
+@onready var animation_player : AnimationPlayer   = $AnimationPlayer
+@onready var navigation_agent : NavigationAgent2D = $NavigationAgent2D
+
+var speed : int = 400
+var acceleration = 15
+
+func _ready() -> void:
+	var random_color: Color = Color8(randi_range(0,255), randi_range(0,255), randi_range(0,255), 255)
+	head.modulate = random_color
+	tail.modulate = random_color
+	_on_navigation_agent_2d_navigation_finished()
+
+func _physics_process(delta: float) -> void:
+	
+	
+
+	var direction : Vector2 = (navigation_agent.get_next_path_position() - global_position).normalized()
+	
+	velocity.x = lerp(velocity.x, speed * direction.x, acceleration * delta)
+	velocity.y = lerp(velocity.y, speed * direction.y * 0.45, acceleration * delta)
+	match velocity.x > 0:
+		true:
+			animation_player.play("swim_right")
+		_:
+			animation_player.play("swim_left")
+
+	move_and_slide()
+
+	
+func _on_navigation_agent_2d_navigation_finished() -> void:
+	navigation_agent.target_position = Vector2(randf_range(100, 1820), randf_range(300,900))
+	
+	
+		
