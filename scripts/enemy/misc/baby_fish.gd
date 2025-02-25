5cf4ac6d67ad6de8a41765500dd1175b8aa9ccd2
SonuTheNecro <156054782+SonuTheNecro@users.noreply.github.com>
2025-02-25 13:28:40 -0600

    add: fish_master

--- scripts/enemy/misc/baby_fish.gd
+++ scripts/enemy/misc/baby_fish.gd
@@ -5,10 +5,12 @@ extends CharacterBody2D
 @onready var garbage_can : Sprite2D               = $garbage_can
 @onready var animation_player : AnimationPlayer   = $AnimationPlayer
 @onready var navigation_agent : NavigationAgent2D = $NavigationAgent2D
+@onready var fish_master      : Node2D            = get_parent()
+
 
 var speed : int = 400
-var acceleration = 15
-
+var acceleration : float = 15
+@export var push_force : int = 40
 func _ready() -> void:
 	var random_color: Color = Color8(randi_range(0,255), randi_range(0,255), randi_range(0,255), 255)
 	navigation_agent.set_avoidance_priority(randf_range(0,1))
@@ -17,38 +19,35 @@ func _ready() -> void:
 	_on_navigation_agent_2d_navigation_finished()
 	var r_scale = randf_range(1.85, 2.15)
 	self.scale = Vector2(r_scale, r_scale)
+	
+	global_position.x = -100 if randi_range(0,1) == 1 else 2000
+	global_position.y = 540
 
 func _physics_process(delta: float) -> void:
 	
-	
-
 	var direction : Vector2 = (navigation_agent.get_next_path_position() - global_position).normalized()
-	
 	navigation_agent.set_velocity(Vector2(
 		lerp(velocity.x, speed * direction.x, acceleration * delta),
 		lerp(velocity.y, speed * direction.y * 0.45, acceleration * delta)
 	))
-
-	
-	
 	match velocity.x > 0:
 		true:
 			animation_player.play("swim_right")
 		_:
 			animation_player.play("swim_left")
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		if collision == null:
+			return
+		if collision.get_collider().is_in_group("drop"):
+			collision.get_collider().apply_central_impulse(-collision.get_normal() * push_force)
 
-	
-
-	
 func _on_navigation_agent_2d_navigation_finished() -> void:
 	navigation_agent.target_position = Vector2(randf_range(100, 1820), randf_range(300,900))
 	
-	
-
 func attacked() -> void:
+	fish_master.current_entities -= 1
 	call_deferred("queue_free")
-		
-
 
 func _on_navigation_agent_2d_velocity_computed(safe_velocity:Vector2) -> void:
 	velocity = safe_velocity
---
+++ scripts/world/classic_world/fish_master.gd
@@ -0,0 +1,30 @@
+extends Node2D
+var spawn_timer : Timer
+var current_entities : int
+@export var wait_time : float
+@export var max_entities : int
+
+const baby_fish = preload("res://scenes/enemy/misc/baby_fish.tscn")
+
+
+func _ready() -> void:
+	spawn_timer = Timer.new()
+	add_child(spawn_timer)
+	spawn_timer.wait_time = wait_time
+	spawn_timer.timeout.connect(spawn_new_enemy)
+	spawn_timer.start()
+	
+
+
+func spawn_new_enemy() -> void:
+	if max_entities <= current_entities:
+		spawn_timer.start()
+		return
+	
+	add_child( baby_fish.instantiate() )
+	spawn_timer.start()
+	current_entities += 1
+
+
+
+
