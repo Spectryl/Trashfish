3e19c90accacf486f90ba52b0d67ff4301c8ef46
SonuTheNecro <156054782+SonuTheNecro@users.noreply.github.com>
2025-02-28 22:05:11 -0600

    cleanup: removed alot of old files

--- scripts/world/classic_world/collision_polygon_2d.gd
+++
@@ -1,8 +0,0 @@
-extends CollisionPolygon2D
-var polygon_2D = Polygon2D.new()
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	add_child(polygon_2D)
-	polygon_2D.set_polygon(polygon)
-	polygon_2D.set_color(Color(0,1,1,0))
