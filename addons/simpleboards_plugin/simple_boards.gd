68b0fffbde7ffbbcbb0ed93730d81d560c3c03f7
SonuTheNecro <156054782+SonuTheNecro@users.noreply.github.com>
2025-03-02 22:09:09 -0600

    test: leaderboard grabbing data

--- addons/simpleboards_plugin/simple_boards.gd
+++ addons/simpleboards_plugin/simple_boards.gd
@@ -5,20 +5,20 @@ extends Node2D
 func _ready():
 	# Set the API key
 	simpleboards.set_api_key("f70edd68-7a2f-4bd1-a84d-4ba99358dda8")
-	#simpleboards.entries_got.connect(_on_entries_got)
-	#simpleboards.entry_sent.connect(_on_entry_sent)
+
 	global.simple_boards = simpleboards
 	
 	# Send a score
-	await simpleboards.send_score_without_id("7dc916e3-eb5b-4bad-0f51-08dd59d342af", "SonuTheNecro", "911", "[]")
+	#await simpleboards.send_score_without_id("7dc916e3-eb5b-4bad-0f51-08dd59d342af", "IHateNiggers", "55", "[]")
 	#await simpleboards.send_score_with_id("7dc916e3-eb5b-4bad-0f51-08dd59d342af", "PlayerName", "12345", "[]", "1")
 	
 	# Get leaderboard entries
-	await simpleboards.get_entries("7dc916e3-eb5b-4bad-0f51-08dd59d342af")
+	#await simpleboards.get_entries("7dc916e3-eb5b-4bad-0f51-08dd59d342af")
 
 func _on_entries_got(entries):
 	for entry in entries:
 		print(entry)
 	
 func _on_entry_sent(entry):
-	print(entry)
+	pass
+	#print(entry)
--- scripts/world/main_menu/leader_board.gd
+++ scripts/world/main_menu/leader_board.gd
@@ -1,9 +1,11 @@
 extends Control
-@onready var menu : Control = get_parent()
+@onready var menu : Control                       = get_parent()
+@onready var vbox: VBoxContainer                  = $ScrollContainer/VBoxContainer
 @onready var title_screen_button  : TextureButton = $title_screen_button
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
-	pass # Replace with function body.
+	global.simple_boards.entries_got.connect(_on_entries_got)
+	await global.simple_boards.get_entries("7dc916e3-eb5b-4bad-0f51-08dd59d342af")
 
 
 
@@ -11,9 +13,17 @@ func _ready() -> void:
 func _on_title_screen_button_pressed() -> void:
 	menu.switch_menu(0)
 
-
 func _on_title_screen_button_mouse_entered() -> void:
 	title_screen_button.set_deferred("modulate", Color8(0,255,255,255))
 	global.sound_master.play("button_hover")
 func _on_title_screen_button_mouse_exited() -> void:
 	title_screen_button.set_deferred("modulate", Color8(255,255,255,255))
+
+
+func _on_entries_got(entries):
+	var count : int = 0
+	for entry in entries:
+		print(entry)
+		vbox.get_child(count).player_name_label.text = entry["playerDisplayName"]
+		vbox.get_child(count).score_label.text       = entry["score"]
+		count += 1
