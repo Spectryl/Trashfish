2886b79e919c898078e2bb5f8cc3182ebcf0f507
SonuTheNecro <156054782+SonuTheNecro@users.noreply.github.com>
2025-03-07 13:52:03 -0600

    add: refresh button to refresh leaderboards

--- scripts/world/main_menu/leader_board.gd
+++ scripts/world/main_menu/leader_board.gd
@@ -2,6 +2,7 @@ extends Control
 @onready var menu : Control                       = get_parent()
 @onready var vbox: VBoxContainer                  = $ScrollContainer/VBoxContainer
 @onready var title_screen_button  : TextureButton = $title_screen_button
+@onready var refresh_button       : TextureButton = $refresh_button
 @onready var player_name_input    : LineEdit      = $player_name_input
 var delay_timer : Timer
 func _ready() -> void:
@@ -34,6 +35,7 @@ func set_up_data(entries):
 		vbox.get_child(count).player_name_label.text = entry["playerDisplayName"]
 		vbox.get_child(count).score_label.text       = entry["score"]
 		count += 1
+	refresh_button.disabled = false
 func delay_timer_check():
 	if online_master.data == null:
 		delay_timer.start()
@@ -42,6 +44,18 @@ func delay_timer_check():
 	delay_timer.queue_free()
 	set_up_data(online_master.data)
 
+func get_data() -> void:
+	delay_timer  = Timer.new()
+	delay_timer .wait_time = 3
+	delay_timer .timeout.connect(delay_timer_check)
+	delay_timer .one_shot = true
+	delay_timer .autostart = false
+	add_child(delay_timer)
+	online_master.get_results()
+	online_master.data = null
+	delay_timer.start()
+
+
 func _on_player_name_input_text_submitted(new_text: String) -> void:
 
 	save_master.save_data.set_value("player", "player_name", new_text)
@@ -49,3 +63,8 @@ func _on_player_name_input_text_submitted(new_text: String) -> void:
 
 func _on_player_name_input_button_pressed() -> void:
 	_on_player_name_input_text_submitted(player_name_input.text)
+
+
+func _on_refresh_button_pressed() -> void:
+	get_data()
+	refresh_button.disabled = true
