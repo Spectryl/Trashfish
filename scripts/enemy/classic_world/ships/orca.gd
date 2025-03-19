ff0876577528e2c5cf322a94566e21b322cc8ec4
SonuTheNecro <156054782+SonuTheNecro@users.noreply.github.com>
2025-03-18 23:49:33 -0500

    test: rage mode activate

--- scripts/enemy/classic_world/ships/orca.gd
+++ scripts/enemy/classic_world/ships/orca.gd
@@ -1,45 +1,15 @@
 extends CharacterBody2D
-@onready var animated_sprite : AnimatedSprite2D = $AnimatedSprite2D
+@onready var body : Sprite2D                    = $body
+@onready var fins : AnimatedSprite2D            = $fins
+@onready var bin  : Sprite2D                    = $bin
+@onready var tail : Sprite2D                    = $tail
+@onready var animation_player : AnimationPlayer = $AnimationPlayer
+@onready var collision : CollisionShape2D       = $CollisionShape2D
+@onready var particle : CPUParticles2D          = $CPUParticles2D
 
-@export var speed : int = 150
-var endPos : Vector2
-var angle : float
-var direction : Vector2
-var parent  : Node2D
-var parentX : int
-var parentY : int
+func _ready():
+	bin.visible = true
 
-var distance
-func _ready() -> void:
-	parent  = get_parent()
-	parentX = parent.global_position.x
-	parentY = parent.global_position.y
-	speed += randi_range(0,50)
-	if parentX == 50:
-		endPos.x = 2000
-		endPos.y =  randi_range(0,700)  + 250
-		global_position.x = -100
-		global_position.y =  parentY
-	else:
-		endPos.x = -100
-		endPos.y =  randi_range(0,700) + 250
-		global_position.x = 2000
-		global_position.y =  parentY
-		animated_sprite.flip_h = true
-	
-	angle = global_position.angle_to_point(endPos)
-	direction = Vector2(cos(angle), sin(angle)) 
-	distance = direction * speed
-	animated_sprite.play("default")
-func _process(delta) -> void:
-	global_position += distance * delta
-	if global_position.x > 2000 or global_position.x < -100:
-		parent.fish_returned += 1
-		call_deferred("queue_free")
-
-
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player"):
-		body.decrease_health()
-	if body.is_in_group("baby_fish"):
-		body.call_deferred("queue_free")
+## turns on the particles when the orca goes anger mode, run this in the animation player
+func turn_on_particles():
+	particle.emitting = true
--- scripts/main/sound_master.gd
+++ scripts/main/sound_master.gd
@@ -25,20 +25,20 @@ func _ready() -> void:
 	# Have to declare the dictionary like this because onready priority moment
 	sound_dictionary["bloop"]        = bloop
 	sound_dictionary["button_hover"] = button_hover
-	sound_dictionary["explosion1"]   = explosion1
 	sound_dictionary["chomp1"]       = chomp1
 	sound_dictionary["chomp2"]       = chomp2
 	sound_dictionary["chomp3"]       = chomp3
+	sound_dictionary["drop_splash"]  = drop_splash
+	sound_dictionary["explosion1"]   = explosion1
 	sound_dictionary["fuse"]         = fuse
 	sound_dictionary["heal"]         = heal
-	sound_dictionary["wave_splash"]  = wave_splash
-	sound_dictionary["drop_splash"]  = drop_splash
-	sound_dictionary["ui_music_test"]= ui_music_test
-	sound_dictionary["ui_sfx_test"]  = ui_sfx_test
 	sound_dictionary["rifle_shot1"]  = rifle_shot1
 	sound_dictionary["rifle_shot2"]  = rifle_shot2
 	sound_dictionary["rifle_shot3"]  = rifle_shot3
 	sound_dictionary["small_chomp"]  = small_chomp
+	sound_dictionary["ui_music_test"]= ui_music_test
+	sound_dictionary["ui_sfx_test"]  = ui_sfx_test
+	sound_dictionary["wave_splash"]  = wave_splash
 	global.sound_master = self
 
 #Plays a sound effect
