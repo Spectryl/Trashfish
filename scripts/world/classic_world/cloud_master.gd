71870fb2455a30859560d4887b2261f44a13974e
Sonu <bhaarat.mohanta@gmail.com>
2025-03-27 16:36:24 -0500

    add: different behaviors for cloudiness

--- scripts/world/classic_world/cloud_master.gd
+++ scripts/world/classic_world/cloud_master.gd
@@ -2,7 +2,18 @@ extends Spawner
 
 const cloud = preload("res://scenes/enemy/misc/cloud.tscn")
 
-
+func _ready() -> void:
+	match randi_range(0,50):
+		0,1,2,3,4,5:
+			print("Today is a cloudy day")
+			wait_time = 1
+		6,7,8,9,10:
+			print("I guess it is not a cloudy day")
+			call_deferred("queue_free")
+		_:
+			print("Normal Weather day")
+			pass
+	super._ready()
 # @Override
 func spawn_new_enemy() -> void:
 	var new_cloud : Node2D = cloud.instantiate()
